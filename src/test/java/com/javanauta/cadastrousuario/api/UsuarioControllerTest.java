d1620b561a11a91267ad9064f59255defdba3eef
Alan Ferreira <131145088+AlanF-Oliveira@users.noreply.github.com>
2026-04-22 18:40:37 -0300

    adicionado teste no metodo de busca de usuarios por email

--- src/test/java/com/javanauta/cadastrousuario/api/UsuarioControllerTest.java
+++ src/test/java/com/javanauta/cadastrousuario/api/UsuarioControllerTest.java
@@ -21,9 +21,9 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
 import static org.mockito.Mockito.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -131,5 +131,19 @@ public class UsuarioControllerTest {
                 .accept(MediaType.APPLICATION_JSON)
         ).andExpect(status().isBadRequest());
         verifyNoInteractions(usuarioService);
+    }
+
+    @Test
+    void deveBuscarDadosDeUsuariosComSucesso() throws Exception {
+
+        mockMvc.perform(get(url)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .param("email", "alanf@gmail.com")
+                .content(json)
+        ).andExpect(status().isOk());
+        verify(usuarioService).buscaDadosUsuario("alanf@gmail.com");
+        verifyNoMoreInteractions(usuarioService);
+
     }
 }
