ebf1772008845090b794d4864d033db961140483
Alan Ferreira <131145088+AlanF-Oliveira@users.noreply.github.com>
2026-04-22 18:16:56 -0300

    adc teste no metodo de gravarUsuarios da controller

--- src/test/java/com/javanauta/cadastrousuario/api/UsuarioControllerTest.java
+++ src/test/java/com/javanauta/cadastrousuario/api/UsuarioControllerTest.java
@@ -8,16 +8,25 @@ import com.javanauta.cadastrousuario.api.request.EnderecoRequestDTO;
 import com.javanauta.cadastrousuario.api.request.EnderecoRequestDTOFixture;
 import com.javanauta.cadastrousuario.api.request.UsuarioRequestDTO;
 import com.javanauta.cadastrousuario.api.request.UsuarioRequestDTOFixture;
+import com.javanauta.cadastrousuario.api.response.EnderecoResponseDTO;
+import com.javanauta.cadastrousuario.api.response.EnderecoResponseDTOFixture;
+import com.javanauta.cadastrousuario.api.response.UsuarioResponseDTO;
+import com.javanauta.cadastrousuario.api.response.UsuarioResponseDTOFixture;
 import com.javanauta.cadastrousuario.business.UsuarioService;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(MockitoExtension.class)
 public class UsuarioControllerTest {
@@ -29,7 +38,7 @@ public class UsuarioControllerTest {
 
     MockMvc mockMvc; //Responsável por fazer o servidor fake
 
-    ObjectMapper objectMapper; //responsável por transformar o DTO em um JSON
+    private final ObjectMapper objectMapper = new ObjectMapper()                        ; //responsável por transformar o DTO em um JSON
 
     private String url;
 
@@ -38,6 +47,10 @@ public class UsuarioControllerTest {
     private EnderecoRequestDTO enderecoRequestDTO;
 
     private String json;
+
+    private EnderecoResponseDTO enderecoResponseDTO;
+
+    private UsuarioResponseDTO usuarioResponseDTO;
 
     @BeforeEach
     void setup() throws JsonProcessingException {
@@ -57,7 +70,35 @@ public class UsuarioControllerTest {
                 "2345674",
                 enderecoRequestDTO
         );
+        enderecoResponseDTO = EnderecoResponseDTOFixture.build(
+                "Rua Spring Boot",
+                23L,
+                "Bairro Java",
+                "Apt 5",
+                "Javaland",
+                "60456543"
+        );
+        usuarioResponseDTO = UsuarioResponseDTOFixture.build(
+                1452L,
+                "Usuario",
+                "alanf@gmail.com",
+                "2345674",
+                enderecoResponseDTO
+        );
         json = objectMapper.writeValueAsString(usuarioRequestDTO);
+    }
 
+    @Test
+    void deveGravarDadosDeUsuarioComSucesso() throws Exception {
+        when(usuarioService.gravarUsuarios(usuarioRequestDTO)).thenReturn(usuarioResponseDTO);
+
+        mockMvc.perform(post(url)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .content(json)
+        ).andExpect(status().isOk());
+
+        verify(usuarioService).gravarUsuarios(usuarioRequestDTO);
+        verifyNoMoreInteractions(usuarioService);
     }
 }
